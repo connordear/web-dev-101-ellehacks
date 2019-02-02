a49db88a5784b93ff10dd4e4011c5d52cb2c5836
Connor Dear <connordear@gmail.com>
2019-02-02 02:01:45 -0500

    updated sound files

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const loops = document.getElementsByTagName("audio");
 let isPlaying = [false, false, false, false];
 
 for (let i = 0; i < soundButtons.length; i++) {
-    let currentButton = soundButtons.item(i);
+    let currentButton = soundButtons[i];
     currentButton.addEventListener("click", function() {
         // First Step
         currentButton.classList.toggle("pushed-button");
@@ -23,5 +23,41 @@ for (let i = 0; i < soundButtons.length; i++) {
         isPlaying[i] = !isPlaying[i];
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SUPPORT FOR FULL BUTTON
+
+const allButton = document.querySelector(".sound-button-2");
+let allOn = false;
+
+allButton.addEventListener("click", function() {
+    allButton.classList.toggle("pushed-button-2");
+    for (let sb of soundButtons) {
+        sb.classList.toggle("hide");
+    }
+    if (allOn) {
+        for (let loop of loops) {
+            loop.pause();
+            loop.load();
+        }
+    } else {
+        for (let loop of loops) {
+            loop.play();
+        }
+    }
+    allOn = !allOn;
+})
 
   
